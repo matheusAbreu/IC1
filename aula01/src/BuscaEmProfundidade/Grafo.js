856965fb653c7a05c75899e856e9dc25d2ea36d0
Matheus Abreu <matheus.moon@gmail.com>
2019-04-08 00:14:29 -0300

    atualizando as estruturas

--- aula01/src/BuscaEmProfundidade/Grafo.js
+++ aula01/src/BuscaEmProfundidade/Grafo.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react';
 import LittleCard from './LittleCard';
 
 //enum Direcao  {baixo= 1, esquerda= 2, cima= 3, direita= 4};
-//var direcao = {baixo: 1, esquerda: 2, cima: 3, direita: 4};
+var direcao = {baixo: 1, esquerda: 2, cima: 3, direita: 4};
+var ligamentos = {
+    dir: 0,
+    nomeDoNo:""        
+}
+
+var no = {
+    nome: "",
+    listaDeLigamentos: [],
+
+    NovoNo: function(newNome)
+    {
+        this.nome = newNome;
+    },
+    AddLigamento: function(novaDir, noRef)
+    {
+        var novo = new ligamentos();
+        novo.dir = novaDir;
+        novo.nomeDoNo = noRef;
+        this.listaDeLigamentos.push(novo);
+    }
+}
 
 export default class Grafo extends Component{
-   
+
     cardGrafo = {
         title: "Grafo",
         text: "Lista de nó",
+        listaDeNos: [],
 
         GeraGrafo: function()
         {
-
+            
         }
     }
     render()
