6c888f2c1ee873b2c9c0af80b1083b6f0f28e65d
Matheus Abreu <matheus.moon@gmail.com>
2019-04-23 04:28:40 -0300

    Gerando automaticamente o grafo utilizado na aulas. Prox passo: inserir os metodos de busca

--- aula01/src/BuscaEmProfundidade/AbasOcultaveis.js
+++ aula01/src/BuscaEmProfundidade/AbasOcultaveis.js
@@ -8,40 +8,44 @@ export default class Aba extends Component
     {
         super(props);
         this.title = props.title;
-        this.text = props.text;
-        this.state = {divClass: 'tab-pane fade hidden',
-                    estadoDiv: DivState.closed};
+        this.index = props.index;
+        this.state = {divClass: 'tab-pane fade show active', 
+                    estadoDiv: DivState.open
+                    };
 
-        this.OcultandoDivs = this.OcultandoDivs.bind();
+        this.OcultandoDivs = this.OcultandoDivs.bind(this);
     }
     OcultandoDivs(e)
     {
-        var teste = this.state.estadoDiv.toString();
-       if(teste === 'Fechada')
-       {
-           this.estadoDiv = DivState.open;
-           this.setState({divClass: 'tab-pane fade show active'});
-       }
-       else
-       {
-           this.estadoDiv = DivState.closed;
-            this.setState({divClass: 'tab-pane fade hidden'});
-       }
+        if(this.index > 1)
+        {
+            if(this.state.estadoDiv === DivState.closed)
+            {
+                this.setState({divClass: 'tab-pane fade show active', estadoDiv:DivState.open});
+                e.target.className = 'nav-link btn btn-outline-primary active';
+            }
+            else
+            {
+                this.setState({divClass: 'tab-pane fade hidden', estadoDiv:DivState.closed});
+                e.target.className = 'nav-link btn btn-outline-primary';
+            }
+        }else
+        {e.target.className = 'nav-link btn btn-outline-primary';}
     }
     render()
     {
         return (
-            <div style={{marginTop: '1%'}}>
+            <div>
                 <ul className='nav nav-tabs' id='myTab' role='tablist'>
                     <li className='nav-item '>
-                        <a className='nav-link btn btn-outline-primary active' id='grafo-tab' data-toggle='tab' href='#grafo' role='tab' 
+                        <a className='nav-link btn btn-outline-primary' id='grafo-tab' data-toggle='tab' href='#grafo' role='tab' 
                         aria-controls='div-tab-grafo' onClick={this.OcultandoDivs} aria-selected='true' >{this.title}</a>
                     </li>
                 </ul>
                 <div className='tab-content' id='myTabContent' >    
                     <div id='div-tab-grafo'  className={this.state.divClass} role='tabpanel' aria-labelledby='grafo-tab' >
                         <div className='row'>
-                            {this.text}
+                            {this.props.text}
                         </div>
                     </div>
                 </div>    
--- aula01/src/BuscaEmProfundidade/Grafo.js
+++ aula01/src/BuscaEmProfundidade/Grafo.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
-import Abas from './AbasOcultaveis';
+import Aba from './AbasOcultaveis';
 import LittleCard from './LittleCard';
 
 //enum Direcao  {baixo= 1, esquerda= 2, cima= 3, direita= 4};
 var direcao = { baixo: "Baixo", esquerda: "Esquerda", cima: "Cima", direita: "Direita" };
+function EscrevendoAlfabeto(numDaLetra)
+{
+    switch (numDaLetra)
+    {
+        case 1: return "A"; 
+        case 2: return "B"; 
+        case 3: return "C";
+        case 4:return "D";
+        case 5:return "E";
+        case 6:return "F";
+        case 7:return "G";
+        case 8:return "H";
+        case 9:return "I";
+        case 10:return "J";
+        case 11:return "L";
+        case 12:return "M";
+        case 13:return "N";
+        case 14:return "O";
+        case 15:return "P";
+        case 16:return "Q";
+        case 17:return "R";
+        case 18:return "S";
+        case 19:return "T";
+        case 20:return "U";
+        case 21:return "V";
+        case 22:return "X";
+        case 23:return "W";
+        case 24:return "Y";
+        case 25:return "Z";
+        default: return ""; 
+    }
+}
 function ligamentos(dir,passNo) {
     this.dir = dir;
     this.no = passNo;
@@ -24,7 +56,7 @@ function no(MyName, lista = new Array()) {
     this.ProcuraNoNasLigacoes = function (nomeDoNo) {
         if (this.listaDeLigamentos.length > 0) {
             for (var i = 0; i < this.listaDeLigamentos.length; i++) {
-                if (this.listaDeLigamentos[i].nome == nomeDoNo)
+                if (this.listaDeLigamentos[i].nome === nomeDoNo)
                     return this.listaDeLigamentos[i];
             }
         }
@@ -62,8 +94,8 @@ export default class Grafo extends Component {
         this.title = props.title;
         this.state = {
             nos: [new no('A'), new no('B'), new no('C'), new no('D'), new no('E'),
-            new no('F'), new no('G'), new no('H'), new no('I'), new no('J'), new no('K'),
-            new no('L'), new no('M'), new no('N'), new no('O'), new no('P'), new no('Q'),
+            new no('F'), new no('G'), new no('H'), new no('I'), new no('J'), new no('L'), 
+            new no('M'), new no('N'), new no('O'), new no('P'), new no('Q'),
             new no('R'), new no('S')],
             noInicial: '',
             noFinal: '',
@@ -74,36 +106,78 @@ export default class Grafo extends Component {
         this.handleTextChangeNoInicial = this.handleTextChangeNoInicial.bind(this);
         this.handleTextChangeNoFinal = this.handleTextChangeNoFinal.bind(this);
         this.ApontandoDirecao = this.ApontandoDirecao.bind(this);
+        this.MontandoGrafoAula = this.MontandoGrafoAula.bind(this);
+        this.DuplaLigacao = this.DuplaLigacao.bind(this);
     }
+    DuplaLigacao(noUm, dir, noDois)
+    {
+        
+        if(this.state.nos.length > 0)
+        {
+            for(var i = 0; i < this.state.nos.length; i++) 
+            {
+                if (this.state.nos[i].nome === noUm.trim().toUpperCase())
+                {
+                    for(var j= 0; j < this.state.nos.length; j++)
+                    {
+                        if(this.state.nos[j].nome === noDois.trim().toUpperCase())
+                        {
+                            this.state.nos[i].AddLigamento(dir, this.state.nos[j]);
+                            this.state.nos[j].AddLigamento(IdentificaDirecaoOposta(dir), this.state.nos[i]);
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+    MontandoGrafoAula(e)
+    {
+        /* 
+            new no('A'), new no('B'), new no('C'), new no('D'), new no('E'),
+            new no('F'), new no('G'), new no('H'), new no('I'), new no('J'), new no('K'),
+            new no('L'), new no('M'), new no('N'), new no('O'), new no('P'), new no('Q'),
+            new no('R'), new no('S')
+        
+       var tempNo;
+       for(var i; i < 19; i++)
+       {
+           //this.state.nos.push(new no(EscrevendoAlfabeto(i+1)));
+           tempNo = new no(EscrevendoAlfabeto(i+1));
+           this.state.nos.push(tempNo);
+       }*/
+       
+       
+       this.DuplaLigacao('A', direcao.direita,'e');
+       this.DuplaLigacao('e', direcao.direita, 'i');
+       this.DuplaLigacao('i', direcao.direita,'n');
+       this.DuplaLigacao('i', direcao.baixo, 'j');
+       this.DuplaLigacao('j', direcao.direita, 'o');
+       this.DuplaLigacao('o', direcao.direita, 'r');
+       this.DuplaLigacao('o', direcao.baixo, 'p');
+       this.DuplaLigacao('p', direcao.direita, 's');
+       this.DuplaLigacao('p', direcao.baixo, 'q');
+       this.DuplaLigacao('q', direcao.esquerda, 'm');
+       this.DuplaLigacao('m', direcao.cima, 'l');
+       this.DuplaLigacao('l', direcao.esquerda, 'g');
+       this.DuplaLigacao('a', direcao.baixo,'B');
+       this.DuplaLigacao('b', direcao.baixo, 'c');
+       this.DuplaLigacao('b', direcao.direita, 'f');
+       this.DuplaLigacao('g', direcao.baixo, 'h');
+       this.DuplaLigacao('h', direcao.esquerda, 'd');
+       this.setState({... this.state.nos});
+    }
+    
     ApontandoDirecao(e)
     {
         this.setState({tempDirecao: e.target.value});
     }
     handleSubmit(e) {
-        var localNoInicio = this.state.noInicial.toUpperCase(), 
-        localNoFinal = this.state.noFinal.toUpperCase();
+             
+        this.DuplaLigacao(this.state.noInicial, this.state.tempDirecao, this.state.noFinal);
         
-        if(this.state.nos.length > 0)
-        {
-            for (var i = 0; i < this.state.nos.length; i++) {
-                if (this.state.nos[i].nome === localNoInicio)
-                {
-                    for (var j= 0; j < this.state.nos.length; j++)
-                    {
-                        if(this.state.nos[j].nome === localNoFinal)
-                        {
-                            this.state.nos[i].AddLigamento(this.state.tempDirecao, this.state.nos[j]);
-                            this.state.nos[j].AddLigamento(IdentificaDirecaoOposta(this.state.tempDirecao), this.state.nos[i]);
-                        }
-                    }
-                    break;
-                }
-            }
-        }
         //Limpando o campo text
-        this.setState({ noInicial: '' });
-        this.setState({ noFinal: '' });
-        this.setState({tempDirecao: 'Escolha a Direcao'});
+        this.setState({ noInicial: '', noFinal: '',tempDirecao: 'Escolha a Direcao' });
         //Removendo o refresh da pag ao submeter o conteudo
         e.preventDefault();
     }
@@ -119,10 +193,17 @@ export default class Grafo extends Component {
         return (
             <div className="container text-center">
                 <h2>{this.props.title}</h2>
-                <form className='input-group' onSubmit={this.handleSubmit}>
+                <div>
+                    <button className='btn btn-outline-primary' onClick={this.MontandoGrafoAula}>Montar Grafo Da Aula</button>
+                </div>
+                <form className='input-group'>
+                    
                     <div className='input-group-prepend'>
-                        <button className='btn btn-outline-primary' type="submit">Montar Grafo Da Aula</button>
+                            <button className='btn btn-outline-primary' type="submit">Inserir No</button>
                     </div>
+                    <input className='form-control' placeholder='Add No (nao funciona ainda)' />
+                </form>
+                <form className='input-group' onSubmit={this.handleSubmit}>
                     <input className='form-control' placeholder='No de Inicio' 
                     style={{ width: '9rem', paddingLeft: '1rem', margin: '0' }} value={this.state.noInicial} 
                     onChange={this.handleTextChangeNoInicial} />
@@ -139,16 +220,17 @@ export default class Grafo extends Component {
                         </div>
                     </div>
 
-                    <input className='form-control' placeholder='No Final' style={{ width: '9rem', paddingLeft: '1rem' }} value={this.state.noFinal} onChange={this.handleTextChangeNoFinal} />
+                    <input className='form-control' placeholder='No Final' 
+                    style={{ width: '9rem', paddingLeft: '1rem' }} value={this.state.noFinal} 
+                    onChange={this.handleTextChangeNoFinal} />
                     <div className='input-group-prepend'>
                         <button className='btn btn-outline-primary' type="submit">Inserir Ligacao</button>
                     </div>
                 </form>
-                <Abas title={this.props.title} text={this.state.nos.map((nos, index) => {
+               
+                <Aba title={this.props.title} index={this.state.nos.length} text={this.state.nos.map((nos, index) => {
                         return <LittleCard key={index} title={nos.nome} text={nos.ImprimaListaDeLigamentos()} />
-                    })} />
-                    
-
+                    })} style={{marginTop: '1%'}} />
             </div>
         );
     }
@@ -156,7 +238,10 @@ export default class Grafo extends Component {
 };
 
 /*function GeraGrafo(myText)
-{
+{   
+                <Aba title={this.props.title} index={this.state.nos.length} text={this.state.nos.map((nos, index) => {
+                        return <LittleCard key={index} title={nos.nome} text={nos.ImprimaListaDeLigamentos()} />
+                    })} style={{marginTop: '1%'}} />
     var temp = new no(), myList = [];
         for(var i = 0; i <10;i++)
         {
