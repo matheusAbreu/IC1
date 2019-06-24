568c6202975a00c15d03663f62e4eb69d5508157
Matheus abreu <matheus.moon@gmail.com>
2019-06-24 18:21:34 -0300

    Busca Gulosa Funcionando!

--- aula02/src/Buscas/js/Gulosa.js
+++ aula02/src/Buscas/js/Gulosa.js
@@ -1,29 +1,56 @@
 import React from 'react';
+import Ordenada from './Ordenada';
 import {No} from './GrafoComPeso';
 
+class ArvorePesada 
+{
+    constructor(meuNo, pesoAteAqui)
+    {
+        this.no = meuNo;
 
-export default class Ordenada extends React.Component
+        this.pesoRelativo = pesoAteAqui;
+        this.arvoreDeCaminha = [];
+    }
+    AddNosNoCaminho(noFilho, pesoDoNoFilho)
+    {
+        var tempNo = new ArvorePesada(noFilho, (pesoDoNoFilho + this.pesoRelativo));
+        this.arvoreDeCaminha.push(tempNo);
+    }
+}
+
+export default class Gulosa extends Ordenada
 {
    constructor(props)
    {
       super(props);
-      this.meuGrafo = undefined;
-
-      this.MontandoArvoreResult = this.MontandoArvoreResult.bind(this);
-      this.IdentificandoMelhorAvaliado = this.IdentificandoMelhorAvaliado.bind(this);
-      this.VerificandoListaFechados = this.VerificandoListaFechados.bind(this);
+      this.arvResult = undefined;
       this.OrganizaListaAbertos = this.OrganizaListaAbertos.bind(this);
+      this.MontandoArvoreGulosaResult = this.MontandoArvoreGulosaResult.bind(this);
+      this.ValorHeuristico = this.ValorHeuristico.bind(this);
+   }
+   ValorHeuristico(noNome)
+   {
+      switch(noNome)
+      {
+         case 'A': return 24;
+         case 'B': return 15;
+         case 'C': return 22;
+         case 'D': return 12;
+         case 'E': return 7;
+         case 'F': return 7;
+         case 'G': return 0;
+      }
    }
    OrganizaListaAbertos()
-    {
-        let temp;
+   {
+      let temp;
         if(this.props.abertos.length > 0)
         {
             for(let i=0;i < this.props.abertos.length; i++)
             {
                 for(let j=this.props.abertos.length-1; j > i; j--)
                 {
-                    if(this.props.abertos[i].pesoRelativo > this.props.abertos[j].pesoRelativo)
+                    if(this.ValorHeuristico(this.props.abertos[i].no.nome) > this.ValorHeuristico(this.props.abertos[j].no.nome))
                     {
                         temp = this.props.abertos[i];
                         this.props.abertos[i] = this.props.abertos[j];
@@ -32,58 +59,59 @@ export default class Ordenada extends React.Component
                 }    
             }
         }
-    }
-    AddListaFechados(novoNo)
-    {
-        if(!this.VerificandoListaFechados(novoNo))
-        {
-            this.props.fechados.push(novoNo);
-        }
-    }
-    VerificandoListaFechados(noVeri)
-    {
-        if(this.props.fechados.length > 0)
-        {
-            for(let i = 0; i < this.props.fechados.length; i++)
-            {
-                if(this.props.fechados[i].nome === noVeri.nome)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-   IdentificandoMelhorAvaliado(noNome)
-   { 
-      //Valores heurística retirados do exemplo em sala
-      switch(noNome)
-      {
-         case 'A': return 0;
-         case 'B': return 13;
-         case 'C': return 15;
-         case 'D': return 7;
-         case 'E': return 10;
-         case 'F': return 10;
-         case 'G': return 0;
-      }
    }
-   MontandoArvoreResult()
+   MontandoArvoreGulosaResult()
    {
-      var resposta = null;
-      var sucesso = false;
-      var arvResult = new No(this.props.abertos[0].nome);
-      var noCandidato = this.props.abertos.shift();
-      
-      /*while(!sucesso)
-      {
-      }*/
+      var noCandidato = new ArvorePesada(this.props.abertos.shift(), 0);
+      var arvResult = noCandidato;
+      var indexcorr = 0;
+      if(noCandidato.no !== undefined){
+         
+          while(true) // xgh brabão aqui
+          {
+              if(noCandidato.no !== undefined && noCandidato.no.nome === this.props.result)
+              {
+                  return arvResult;
+              }
+              else
+              {
+                  if(!this.VerificandoListaFechados(noCandidato.no))
+                  {
+                      this.AddListaFechados(noCandidato.no);
+                      for(let i=0; i < noCandidato.no.listaLigacao.length ;i++)
+                      {
+                          if(!this.VerificandoListaFechados(noCandidato.no.listaLigacao[i].noLigado))
+                          {
+                              noCandidato.AddNosNoCaminho(noCandidato.no.listaLigacao[i].noLigado,noCandidato.no.listaLigacao[i].pesoLigacao);
+                              this.AddListaAbertos(noCandidato.arvoreDeCaminha[i - indexcorr]);
+                          }
+                          else
+                          {
+                              indexcorr++;
+                          }
+                      }
+                  }
+                  indexcorr = 0;
+                  this.OrganizaListaAbertos();
+              }
+              if(this.props.abertos.length > 0)
+              {
+                  noCandidato = this.props.abertos.shift();
+              }
+              else
+              {
+                  break;
+              }
+          }
+      }
+      return arvResult;  
    }
    render()
    { 
+      this.arvResult = this.MontandoArvoreGulosaResult();
       return(
          <div>
-            Hello!
+            {this.ImprimindoArvorePesada(this.arvResult)}
          </div>
       );
    }
